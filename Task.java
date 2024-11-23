5c83b23e89390ba11544df12a0323a2d902dd0c3
djRemskii <jeremyong@cpp.edu>
2024-11-22 21:04:19 -0800

    Created Task classes, developed Task class itself

---
+++ Task.java
@@ -0,0 +1,55 @@
+public class Task {
+    private String name;
+    private String type;
+    private float startTime;
+    private float duration;
+    private int date; 
+
+    public Task(String name, String type, float startTime, float duration, int date){
+        this.name = name;
+        this.type = type;
+        this.startTime = startTime;
+        this.duration = duration;
+        this.date = date;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getType(){
+        return type;
+    }
+
+    public float getStartTime(){
+        return startTime;
+    }
+
+    public float getDuration(){
+        return duration;
+    }
+
+    public int getDate(){
+        return date;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setType(String type){
+        this.type = type;
+    }
+
+    public void setStartTime(float startTime){
+        this.startTime = startTime;
+    }
+
+    public void setDuration(float duration){
+        this.duration = duration;
+    }
+
+    public void setDate(int date){
+        this.date = date;
+    }
+}
---
+++ TransientTask.java
@@ -0,0 +1,3 @@
+public class TransientTask extends Task {
+    
+}
